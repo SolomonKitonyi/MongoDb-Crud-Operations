7fd25a70c27179055b26617250e5f8c935999a4f
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-18 17:22:31 +0300

    Mongo crud operations

---
+++ index.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+ .then(()=>console.log("Connected To MongoDb....."))
+ .catch(err => console.log("Error",err));
+
+ const courseSchema = mongoose.Schema({
+     name:String,
+     author:String,
+     tags: [String],
+     date:{type:Date,default:Date.now},
+     isPublished:Boolean
+ });
+const Course = mongoose.model('Course',courseSchema);
+
+async function createCourse(){
+const course = new Course({
+    name:'Angular Course',
+    author:'Solomon',
+    tags:['Angular','Fronted'],
+    isPublished:true
+});
+
+const result = await course.save();
+console.log(result);
+};
+
+async function getCourses(){
+//comparison operators in mongodb
+//eq (equal)
+//ne (not equal)
+//gt (greater than)
+//gte (greater than or equal to )
+//lt (less than)
+//lte (less than or equal to )
+//in 
+//nin (not in)
+
+    const courses = await Course.
+    //find({author:'Solomon',isPublished:true})
+    //find({price:{$gte:10,$lt:20}})
+    find({price:{$in: [10,15,20]}})
+    .limit(10)
+    .sort({name:1})
+    .select({name:1,tags:1});
+    console.log(courses);
+}
+getCourses();
